8da121f8280bc8e9f6c99c427c919c4ce8e72252
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-04 00:46:44 +0200

    :oden:Syntax fixes!

--- src/sql/manhattan.sql
+++ src/sql/manhattan.sql
@@ -1,155 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2017 at 11:44 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Table structure for table `patient`
---
 
-CREATE TABLE IF NOT EXISTS `patient` (
-
-		`patient_id` int(11) NOT NULL AUTO_INCREMENT,
-		`name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`surname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`town` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`street_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`number` int(11) NOT NULL,
-		`postalcode` int(11) NOT NULL,
-		`age` int(2) NOT NULL,
-		PRIMARY KEY (`patient_id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `doctor`
+-- Database: `Manhattan`
 --
 
-CREATE TABLE IF NOT EXISTS `doctor` (
-
-		`doctor_id` int(11) NOT NULL AUTO_INCREMENT,
-		`name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`surname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`specialty` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`experience_years` int(2) NOT NULL,
-		PRIMARY KEY (`doctor_id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10;
-
---
--- Table structure for table `pharmaceutical_company`
---
-
-CREATE TABLE IF NOT EXISTS `pharmaceutical_company` (
-
-		`pharmaceutical_company_id` int(11) NOT NULL AUTO_INCREMENT,
-		`name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`phone_number` int(15) NOT NULL,
-		PRIMARY KEY (`pharmaceutical_company_id`),
-		UNIQUE KEY `name` (`name`),
-		UNIQUE KEY `phone_number` (`phone_number`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10;
-
---
--- Table structure for table `drug`
---
-
-CREATE TABLE IF NOT EXISTS `drug` (
-
-		`drug_id` int(11) NOT NULL AUTO_INCREMENT,
-		`name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`formula` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-		PRIMARY KEY (`drug_id`),
-		UNIQUE KEY `name` (`name`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10;
-
---
--- Table structure for table `pharmacy`
---
-
-CREATE TABLE IF NOT EXISTS `pharmacy` (
-
-		`pharmacy_id` int(11) NOT NULL AUTO_INCREMENT,
-		`name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`town` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`street_name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		`number` int(11) NOT NULL,
-		`postalcode` int(11) NOT NULL,
-		`phone_number` int(15) NOT NULL,
-		PRIMARY KEY (`pharmacy_id`),
-		UNIQUE KEY `name` (`name`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10;
-
---
--- Table structure for table `seen_by`
---
-
-CREATE TABLE IF NOT EXISTS `seen_by` (
-
-		`patient_id` int(11) NOT NULL,
-		`doctor_id` int(11) NOT NULL,
-		PRIMARY KEY (`patient_id`),
-		KEY `doctor_id` (`doctor_id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `sell`
---
-
-CREATE TABLE IF NOT EXISTS `sell` (
-
-		`pharmacy_id` int(11) NOT NULL,
-		`drug_id` int(11) NOT NULL,
-		`price` int(10) NOT NULL,
-		PRIMARY KEY (`pharmacy_id`, `drug_id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `make`
---
-
-CREATE TABLE IF NOT EXISTS `make` (
-
-		`drug_id` int(11) NOT NULL,
-		`pharmaceutical_company_id` int(11) NOT NULL,
-		PRIMARY KEY (`drug_id`),
-		KEY `pharmaceutical_company_id` (`pharmaceutical_company_id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `prescription`
---
-
-CREATE TABLE IF NOT EXISTS `prescription` (
-
-		`patient_id` int(11) NOT NULL,
-		`doctor_id` int(11) NOT NULL,
-		`drug_id` int(11) NOT NULL,
-		`date` date NOT NULL,
-		`quantity` int(10) NOT NULL,
-		PRIMARY KEY (`patient_id`, `doctor_id`, `drug_id`)
-
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `contract`
 --
 
-CREATE TABLE IF NOT EXISTS `contract` (
+CREATE TABLE `contract` (
+  `pharmacy_id` int(11) NOT NULL,
+  `pharmaceutical_company_id` int(11) NOT NULL,
+  `drug_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `text` varchar(50) NOT NULL,
+  `supervisor` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-		`pharmacy_id` int(11) NOT NULL,
-		`pharmaceutical_company_id` int(11) NOT NULL,
-		`drug_id` int(11) NOT NULL,
-		`start_date` date NOT NULL,
-		`end_date` date NOT NULL,
-		`text` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-		`supervisor` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-		PRIMARY KEY (`pharmacy_id`, `pharmaceutical_company_id`),
-		KEY `drug_id` (`drug_id`)
+-- --------------------------------------------------------
 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doctor_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(20) NOT NULL,
+  `specialty` varchar(20) NOT NULL,
+  `experience_years` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drug`
+--
+
+CREATE TABLE `drug` (
+  `drug_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `formula` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `make`
+--
+
+CREATE TABLE `make` (
+  `drug_id` int(11) NOT NULL,
+  `pharmaceutical_company_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patient_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(20) NOT NULL,
+  `town` varchar(20) NOT NULL,
+  `street_name` varchar(20) NOT NULL,
+  `number` int(11) NOT NULL,
+  `postalcode` int(11) NOT NULL,
+  `age` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pharmaceutical_company`
+--
+
+CREATE TABLE `pharmaceutical_company` (
+  `pharmaceutical_company_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `phone_number` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pharmacy`
+--
+
+CREATE TABLE `pharmacy` (
+  `pharmacy_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `town` varchar(20) NOT NULL,
+  `street_name` varchar(20) NOT NULL,
+  `number` int(11) NOT NULL,
+  `postalcode` int(11) NOT NULL,
+  `phone_number` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `drug_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `quantity` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seen_by`
+--
+
+CREATE TABLE `seen_by` (
+  `patient_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sell`
+--
+
+CREATE TABLE `sell` (
+  `pharmacy_id` int(11) NOT NULL,
+  `drug_id` int(11) NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contract`
+--
+ALTER TABLE `contract`
+  ADD PRIMARY KEY (`pharmacy_id`,`pharmaceutical_company_id`),
+  ADD KEY `drug_id` (`drug_id`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doctor_id`);
+
+--
+-- Indexes for table `drug`
+--
+ALTER TABLE `drug`
+  ADD PRIMARY KEY (`drug_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `make`
+--
+ALTER TABLE `make`
+  ADD PRIMARY KEY (`drug_id`),
+  ADD KEY `pharmaceutical_company_id` (`pharmaceutical_company_id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patient_id`);
+
+--
+-- Indexes for table `pharmaceutical_company`
+--
+ALTER TABLE `pharmaceutical_company`
+  ADD PRIMARY KEY (`pharmaceutical_company_id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `phone_number` (`phone_number`);
+
+--
+-- Indexes for table `pharmacy`
+--
+ALTER TABLE `pharmacy`
+  ADD PRIMARY KEY (`pharmacy_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`patient_id`,`doctor_id`,`drug_id`);
+
+--
+-- Indexes for table `seen_by`
+--
+ALTER TABLE `seen_by`
+  ADD PRIMARY KEY (`patient_id`),
+  ADD KEY `doctor_id` (`doctor_id`);
+
+--
+-- Indexes for table `sell`
+--
+ALTER TABLE `sell`
+  ADD PRIMARY KEY (`pharmacy_id`,`drug_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
