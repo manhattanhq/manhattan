f7930533a1108701aefd16fdb941ae9ccf8d84eb
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-08 06:42:38 +0200

    :tada:App front page assets released!

---
+++ src/assets/lib/js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+	"use strict";
+
+	// Preloader
+	var preloader = $('.preloader');
+	$(window).load(function() {
+		preloader.remove();
+	});
+
+	// Navbar Collapse Button
+
+	$(".button-collapse").sideNav();
+
+	// Slider
+
+	$('.slider').slider({
+		full_width: true
+	});
+
+	// Full Screen Parallax
+
+	var slideHeight = $(window).height();
+	$('.full-screen').css('height', slideHeight);
+	$(window).resize(function() {
+		$('.full-screen').css('height', slideHeight);
+	});
+
+	// Navbar-Collapse (when scrolling)
+
+	$(window).scroll(function() {
+		if ($("header").offset().top > 50) {
+			$("nav").addClass("top-nav-collapse");
+		} else {
+			$("nav").removeClass("top-nav-collapse");
+		}
+	});
+
+	// Page Scroll
+
+	$(function() {
+		$('a.page-scroll').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+	// Owl Carousel (Testimonials)
+
+	$("#owl-testimonials").owlCarousel({
+
+		navigation: false, // hide next and prev buttons
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		singleItem: true
+
+	});
+
+
+});
