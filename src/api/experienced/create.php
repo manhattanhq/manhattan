6cb1fe7da5451b680a914aa1dc3cd8a56d5fe435
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-09 02:41:44 +0200

    :sake:(stable)Experienced-create released!

---
+++ src/api/experienced/create.php
@@ -0,0 +1,32 @@
+<?php
+// include database and object file
+include_once '../config/database.php';
+include_once '../objects/experienced.php';
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+// instantiate experienced object
+$experienced = new Experienced($db);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// set experienced property values
+$experienced->name = $data->name;
+$experienced->surname = $data->surname;
+$experienced->specialty = $data->specialty;
+$experienced->experience_years = $data->experience_years;
+$experienced->created = date('Y-m-d H:i:s');
+
+// create the experienced
+if($experienced->create()){
+    echo "experienced was created.";
+}
+
+// if unable to create the experienced, tell the user
+else{
+    echo "Unable to create experienced.";
+}
+?>
