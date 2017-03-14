a6145f5777a8571552a2182a5cd8d705b06f835d
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-14 02:19:43 +0200

    :rice_cracker:(fix)Access bug resolved!

--- src/api/config/database.php
+++ src/api/config/database.php
@@ -1,23 +1,23 @@
 <?php
 class Database{
 
-    // specify database credentials
-    private $host = "localhost";
-    private $db_name = "manhattan";
-    private $username = "root";
-    private $password = "123";
-    public $conn;
+	// specify database credentials
+	private $host = "localhost";
+	private $db_name = "manhattan";
+	private $username = "root";
+	private $password = "";
+	public $conn;
 
-    // get the database connection
-    public function getConnection(){ $this->conn = null;
+	// get the database connection
+	public function getConnection(){ $this->conn = null;
 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
+		try{
+			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+		}catch(PDOException $exception){
+			echo "Connection error: " . $exception->getMessage();
+		}
 
-        return $this->conn;
-    }
+		return $this->conn;
+	}
 }
 ?>
