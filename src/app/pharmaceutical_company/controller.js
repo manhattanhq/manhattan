ca693fbdfc37bf479d7f4914b40369881e692557
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-18 00:17:33 +0200

    :sparkles:(update)Company controller XO-fied!

--- src/app/pharmaceutical_company/controller.js
+++ src/app/pharmaceutical_company/controller.js
@@ -1,141 +1,133 @@
-app.controller('productsCtrl', function($scope, $http) {
+app.controller('productsCtrl', function ($scope, $http) {
+	// delete pharmaceutical_company
+  $scope.deleteProduct = function (pharmaceutical_company_id) {
+		// ask the user if he is sure to delete the record
+    if (confirm('Are you sure?')) {
+      $http({
+        method: 'POST',
+        data: {
+          pharmaceutical_company_id: pharmaceutical_company_id
+        },
+        url: 'api/pharmaceutical_company/delete.php'
+      }).then(function successCallback(response) {
+				// tell the user pharmaceutical_company was deleted
+        Materialize.toast(response.data, 4000);
 
-    // delete pharmaceutical_company
-    $scope.deleteProduct = function(pharmaceutical_company_id){
-
-        // ask the user if he is sure to delete the record
-        if(confirm("Are you sure?")){
-
-            $http({
-                method: 'POST',
-                data: { 'pharmaceutical_company_id' : pharmaceutical_company_id },
-                url: 'api/pharmaceutical_company/delete.php'
-            }).then(function successCallback(response) {
-
-                // tell the user pharmaceutical_company was deleted
-                Materialize.toast(response.data, 4000);
-
-                // refresh the list
-                $scope.getAll();
-            });
-        }
+				// refresh the list
+        $scope.getAll();
+      });
     }
+  };
 
-    // update pharmaceutical_company record / save changes
-    $scope.updateProduct = function(){
-        $http({
-            method: 'POST',
-            data: {
-                'pharmaceutical_company_id' : $scope.pharmaceutical_company_id,
-                'name' : $scope.name,
-                'phone_number' : $scope.phone_number
-            },
-            url: 'api/pharmaceutical_company/update.php'
-        }).then(function successCallback(response) {
+	// update pharmaceutical_company record / save changes
+  $scope.updateProduct = function () {
+    $http({
+      method: 'POST',
+      data: {
+        pharmaceutical_company_id: $scope.pharmaceutical_company_id,
+        name: $scope.name,
+        phone_number: $scope.phone_number
+      },
+      url: 'api/pharmaceutical_company/update.php'
+    }).then(function successCallback(response) {
+			// tell the user pharmaceutical_company record was updated
+      Materialize.toast(response.data, 4000);
 
-            // tell the user pharmaceutical_company record was updated
-            Materialize.toast(response.data, 4000);
+			// close modal
+      $('#modal-pharmaceutical_company-form').modal('close');
 
-            // close modal
-            $('#modal-pharmaceutical_company-form').modal('close');
+			// clear modal content
+      $scope.clearForm();
 
-            // clear modal content
-            $scope.clearForm();
+			// refresh the pharmaceutical_company list
+      $scope.getAll();
+    });
+  };
 
-            // refresh the pharmaceutical_company list
-            $scope.getAll();
-        });
-    }
+	// retrieve record to fill out the form
+  $scope.readOne = function (pharmaceutical_company_id) {
+		// change modal title
+    $('#modal-pharmaceutical_company-title').text('Edit pharmaceutical_company');
 
-    // retrieve record to fill out the form
-    $scope.readOne = function(pharmaceutical_company_id){
+		// show udpate pharmaceutical_company button
+    $('#btn-update-pharmaceutical_company').show();
 
-        // change modal title
-        $('#modal-pharmaceutical_company-title').text("Edit pharmaceutical_company");
+		// show create pharmaceutical_company button
+    $('#btn-create-pharmaceutical_company').hide();
 
-        // show udpate pharmaceutical_company button
-        $('#btn-update-pharmaceutical_company').show();
+		// post pharmaceutical_company_id of pharmaceutical_company to be edited
+    $http({
+      method: 'POST',
+      data: {
+        pharmaceutical_company_id: pharmaceutical_company_id
+      },
+      url: 'api/pharmaceutical_company/read_one.php'
+    }).then(function successCallback(response) {
+				// put the values in form
+      $scope.pharmaceutical_company_id = response.data[0].pharmaceutical_company_id;
+      $scope.name = response.data[0].name;
+      $scope.phone_number = response.data[0].phone_number;
 
-        // show create pharmaceutical_company button
-        $('#btn-create-pharmaceutical_company').hide();
+				// show modal
+      $('#modal-pharmaceutical_company-form').modal('open');
+    })
+			.error(function (data, status, headers, config) {
+  Materialize.toast('Unable to retrieve record.', 4000);
+});
+  };
 
-        // post pharmaceutical_company_id of pharmaceutical_company to be edited
-        $http({
-            method: 'POST',
-            data: { 'pharmaceutical_company_id' : pharmaceutical_company_id },
-            url: 'api/pharmaceutical_company/read_one.php'
-        }).then(function successCallback(response) {
+	// read pharmaceutical_companyS
+  $scope.getAll = function () {
+    $http({
+      method: 'GET',
+      url: 'api/pharmaceutical_company/read.php'
+    }).then(function successCallback(response) {
+      $scope.names = response.data.records;
+    });
+  };
 
-            // put the values in form
-            $scope.pharmaceutical_company_id = response.data[0]["pharmaceutical_company_id"];
-            $scope.name = response.data[0]["name"];
-            $scope.phone_number = response.data[0]["phone_number"];
+  $scope.showCreateForm = function () {
+		// clear form
+    $scope.clearForm();
 
-            // show modal
-            $('#modal-pharmaceutical_company-form').modal('open');
-        })
-        .error(function(data, status, headers, config){
-            Materialize.toast('Unable to retrieve record.', 4000);
-        });
-    }
+		// change modal title
+    $('#modal-pharmaceutical_company-title').text('Create New pharmaceutical_company');
 
-    // read pharmaceutical_companyS
-    $scope.getAll = function(){
-        $http({
-            method: 'GET',
-            url: 'api/pharmaceutical_company/read.php'
-        }).then(function successCallback(response) {
-            $scope.names = response.data.records;
-        });
-    }
+		// hide update pharmaceutical_company button
+    $('#btn-update-pharmaceutical_company').hide();
 
-    $scope.showCreateForm = function(){
+		// show create pharmaceutical_company button
+    $('#btn-create-pharmaceutical_company').show();
+  };
 
-        // clear form
-        $scope.clearForm();
+	// clear variable / form values
+  $scope.clearForm = function () {
+    $scope.pharmaceutical_company_id = '';
+    $scope.name = '';
+    $scope.phone_number = '';
+  };
 
-        // change modal title
-        $('#modal-pharmaceutical_company-title').text("Create New pharmaceutical_company");
+	// create new pharmaceutical_company
+  $scope.createProduct = function () {
+    $http({
+      method: 'POST',
+      data: {
+        name: $scope.name,
+        phone_number: $scope.phone_number
+      },
+      url: 'api/pharmaceutical_company/create.php'
+    }).then(function successCallback(response) {
+			// tell the user new pharmaceutical_company was created
+      Materialize.toast(response.data, 4000);
 
-        // hide update pharmaceutical_company button
-        $('#btn-update-pharmaceutical_company').hide();
+			// close modal
+      $('#modal-pharmaceutical_company-form').modal('close');
 
-        // show create pharmaceutical_company button
-        $('#btn-create-pharmaceutical_company').show();
+			// clear modal content
+      $scope.clearForm();
 
-    }
-
-    // clear variable / form values
-    $scope.clearForm = function(){
-        $scope.pharmaceutical_company_id = "";
-        $scope.name = "";
-        $scope.phone_number = "";
-    }
-
-    // create new pharmaceutical_company
-    $scope.createProduct = function(){
-
-        $http({
-            method: 'POST',
-            data: {
-                'name' : $scope.name,
-                'phone_number' : $scope.phone_number
-            },
-            url: 'api/pharmaceutical_company/create.php'
-        }).then(function successCallback(response) {
-
-            // tell the user new pharmaceutical_company was created
-            Materialize.toast(response.data, 4000);
-
-            // close modal
-            $('#modal-pharmaceutical_company-form').modal('close');
-
-            // clear modal content
-            $scope.clearForm();
-
-            // refresh the list
-            $scope.getAll();
-        });
-    }
-
+			// refresh the list
+      $scope.getAll();
+    });
+  };
 });
