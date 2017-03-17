8f862968483dd2b05e06242ad2efd9c5986f4f8d
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-18 00:12:55 +0200

    :fish_cake:(update)Patient controller XO-fied!

--- src/app/patient/controller.js
+++ src/app/patient/controller.js
@@ -1,165 +1,157 @@
-app.controller('productsCtrl', function($scope, $http) {
+app.controller('productsCtrl', function ($scope, $http) {
+	// delete patient
+  $scope.deleteProduct = function (patient_id) {
+		// ask the user if he is sure to delete the record
+    if (confirm('Are you sure?')) {
+      $http({
+        method: 'POST',
+        data: {
+          patient_id: patient_id
+        },
+        url: 'api/patient/delete.php'
+      }).then(function successCallback(response) {
+				// tell the user patient was deleted
+        Materialize.toast(response.data, 4000);
 
-    // delete patient
-    $scope.deleteProduct = function(patient_id){
-
-        // ask the user if he is sure to delete the record
-        if(confirm("Are you sure?")){
-
-            $http({
-                method: 'POST',
-                data: { 'patient_id' : patient_id },
-                url: 'api/patient/delete.php'
-            }).then(function successCallback(response) {
-
-                // tell the user patient was deleted
-                Materialize.toast(response.data, 4000);
-
-                // refresh the list
-                $scope.getAll();
-            });
-        }
+				// refresh the list
+        $scope.getAll();
+      });
     }
+  };
 
-    // update patient record / save changes
-    $scope.updateProduct = function(){
-        $http({
-            method: 'POST',
-            data: {
-                'patient_id' : $scope.patient_id,
-                'name' : $scope.name,
-                'surname' : $scope.surname,
-                'town' : $scope.town,
-                'street_name' : $scope.street_name,
-                'number' : $scope.number,
-                'postalcode' : $scope.postalcode,
-                'age' : $scope.age,
-                'doctor_id' : $scope.doctor_id
-            },
-            url: 'api/patient/update.php'
-        }).then(function successCallback(response) {
+	// update patient record / save changes
+  $scope.updateProduct = function () {
+    $http({
+      method: 'POST',
+      data: {
+        patient_id: $scope.patient_id,
+        name: $scope.name,
+        surname: $scope.surname,
+        town: $scope.town,
+        street_name: $scope.street_name,
+        number: $scope.number,
+        postalcode: $scope.postalcode,
+        age: $scope.age,
+        doctor_id: $scope.doctor_id
+      },
+      url: 'api/patient/update.php'
+    }).then(function successCallback(response) {
+			// tell the user patient record was updated
+      Materialize.toast(response.data, 4000);
 
-            // tell the user patient record was updated
-            Materialize.toast(response.data, 4000);
+			// close modal
+      $('#modal-patient-form').modal('close');
 
-            // close modal
-            $('#modal-patient-form').modal('close');
+			// clear modal content
+      $scope.clearForm();
 
-            // clear modal content
-            $scope.clearForm();
+			// refresh the patient list
+      $scope.getAll();
+    });
+  };
 
-            // refresh the patient list
-            $scope.getAll();
-        });
-    }
+	// retrieve record to fill out the form
+  $scope.readOne = function (patient_id) {
+		// change modal title
+    $('#modal-patient-title').text('Edit patient');
 
-    // retrieve record to fill out the form
-    $scope.readOne = function(patient_id){
+		// show udpate patient button
+    $('#btn-update-patient').show();
 
-        // change modal title
-        $('#modal-patient-title').text("Edit patient");
+		// show create patient button
+    $('#btn-create-patient').hide();
 
-        // show udpate patient button
-        $('#btn-update-patient').show();
+		// post patient_id of patient to be edited
+    $http({
+      method: 'POST',
+      data: {
+        patient_id: patient_id
+      },
+      url: 'api/patient/read_one.php'
+    }).then(function successCallback(response) {
+				// put the values in form
+      $scope.patient_id = response.data[0].patient_id;
+      $scope.name = response.data[0].name;
+      $scope.surname = response.data[0].surname;
+      $scope.town = response.data[0].town;
+      $scope.street_name = response.data[0].street_name;
+      $scope.number = response.data[0].number;
+      $scope.postalcode = response.data[0].postalcode;
+      $scope.age = response.data[0].age;
+      $scope.doctor_id = response.data[0].doctor_id;
 
-        // show create patient button
-        $('#btn-create-patient').hide();
+				// show modal
+      $('#modal-patient-form').modal('open');
+    })
+			.error(function (data, status, headers, config) {
+  Materialize.toast('Unable to retrieve record.', 4000);
+});
+  };
 
-        // post patient_id of patient to be edited
-        $http({
-            method: 'POST',
-            data: { 'patient_id' : patient_id },
-            url: 'api/patient/read_one.php'
-        }).then(function successCallback(response) {
+	// read patientS
+  $scope.getAll = function () {
+    $http({
+      method: 'GET',
+      url: 'api/patient/read.php'
+    }).then(function successCallback(response) {
+      $scope.names = response.data.records;
+    });
+  };
 
-            // put the values in form
-            $scope.patient_id = response.data[0]["patient_id"];
-            $scope.name = response.data[0]["name"];
-            $scope.surname = response.data[0]["surname"];
-            $scope.town = response.data[0]["town"];
-            $scope.street_name = response.data[0]["street_name"];
-            $scope.number = response.data[0]["number"];
-            $scope.postalcode = response.data[0]["postalcode"];
-            $scope.age = response.data[0]["age"];
-            $scope.doctor_id = response.data[0]["doctor_id"];
+  $scope.showCreateForm = function () {
+		// clear form
+    $scope.clearForm();
 
-            // show modal
-            $('#modal-patient-form').modal('open');
-        })
-        .error(function(data, status, headers, config){
-            Materialize.toast('Unable to retrieve record.', 4000);
-        });
-    }
+		// change modal title
+    $('#modal-patient-title').text('Create New patient');
 
-    // read patientS
-    $scope.getAll = function(){
-        $http({
-            method: 'GET',
-            url: 'api/patient/read.php'
-        }).then(function successCallback(response) {
-            $scope.names = response.data.records;
-        });
-    }
+		// hide update patient button
+    $('#btn-update-patient').hide();
 
-    $scope.showCreateForm = function(){
+		// show create patient button
+    $('#btn-create-patient').show();
+  };
 
-        // clear form
-        $scope.clearForm();
+	// clear variable / form values
+  $scope.clearForm = function () {
+    $scope.patient_id = '';
+    $scope.name = '';
+    $scope.surname = '';
+    $scope.town = '';
+    $scope.street_name = '';
+    $scope.number = '';
+    $scope.postalcode = '';
+    $scope.age = '';
+    $scope.doctor_id = '';
+  };
 
-        // change modal title
-        $('#modal-patient-title').text("Create New patient");
+	// create new patient
+  $scope.createProduct = function () {
+    $http({
+      method: 'POST',
+      data: {
+        name: $scope.name,
+        surname: $scope.surname,
+        town: $scope.town,
+        street_name: $scope.street_name,
+        number: $scope.number,
+        postalcode: $scope.postalcode,
+        age: $scope.age,
+        doctor_id: $scope.doctor_id
+      },
+      url: 'api/patient/create.php'
+    }).then(function successCallback(response) {
+			// tell the user new patient was created
+      Materialize.toast(response.data, 4000);
 
-        // hide update patient button
-        $('#btn-update-patient').hide();
+			// close modal
+      $('#modal-patient-form').modal('close');
 
-        // show create patient button
-        $('#btn-create-patient').show();
+			// clear modal content
+      $scope.clearForm();
 
-    }
-
-    // clear variable / form values
-    $scope.clearForm = function(){
-        $scope.patient_id = "";
-        $scope.name = "";
-        $scope.surname = "";
-        $scope.town = "";
-        $scope.street_name = "";
-        $scope.number = "";
-        $scope.postalcode = "";
-        $scope.age = "";
-        $scope.doctor_id = "";
-    }
-
-    // create new patient
-    $scope.createProduct = function(){
-
-        $http({
-            method: 'POST',
-            data: {
-                'name' : $scope.name,
-                'surname' : $scope.surname,
-                'town' : $scope.town,
-                'street_name' : $scope.street_name,
-                'number' : $scope.number,
-                'postalcode' : $scope.postalcode,
-                'age' : $scope.age,
-                'doctor_id' : $scope.doctor_id
-            },
-            url: 'api/patient/create.php'
-        }).then(function successCallback(response) {
-
-            // tell the user new patient was created
-            Materialize.toast(response.data, 4000);
-
-            // close modal
-            $('#modal-patient-form').modal('close');
-
-            // clear modal content
-            $scope.clearForm();
-
-            // refresh the list
-            $scope.getAll();
-        });
-    }
-
+			// refresh the list
+      $scope.getAll();
+    });
+  };
 });
