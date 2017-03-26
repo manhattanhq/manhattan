632c482bd9ca4f075e2f7caab71d7862bfc8b64e
Klaus Sinani <klauscfhq@protonmail.com>
2017-03-26 22:31:22 +0300

    :cake:(update)Main app  XO-fied!

--- src/assets/lib/js/main.js
+++ src/assets/lib/js/main.js
@@ -1,63 +1,60 @@
-$(document).ready(function() {
-
-	"use strict";
+$(document).ready(function () {
+  'use strict';
 
 	// Preloader
-	var preloader = $('.preloader');
-	$(window).load(function() {
-		preloader.remove();
-	});
+  var preloader = $('.preloader');
+  $(window).load(function () {
+    preloader.remove();
+  });
 
 	// Navbar Collapse Button
 
-	$(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
 
 	// Slider
 
-	$('.slider').slider({
-		full_width: true
-	});
+  $('.slider').slider({
+    full_width: true
+  });
 
 	// Full Screen Parallax
 
-	var slideHeight = $(window).height();
-	$('.full-screen').css('height', slideHeight);
-	$(window).resize(function() {
-		$('.full-screen').css('height', slideHeight);
-	});
+  var slideHeight = $(window).height();
+  $('.full-screen').css('height', slideHeight);
+  $(window).resize(function () {
+    $('.full-screen').css('height', slideHeight);
+  });
 
 	// Navbar-Collapse (when scrolling)
 
-	$(window).scroll(function() {
-		if ($("header").offset().top > 50) {
-			$("nav").addClass("top-nav-collapse");
-		} else {
-			$("nav").removeClass("top-nav-collapse");
-		}
-	});
+  $(window).scroll(function () {
+    if ($('header').offset().top > 50) {
+      $('nav').addClass('top-nav-collapse');
+    } else {
+      $('nav').removeClass('top-nav-collapse');
+    }
+  });
 
 	// Page Scroll
 
-	$(function() {
-		$('a.page-scroll').bind('click', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+  $(function () {
+    $('a.page-scroll').bind('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+  });
 
 	// Owl Carousel (Testimonials)
 
-	$("#owl-testimonials").owlCarousel({
+  $('#owl-testimonials').owlCarousel({
 
-		navigation: false, // hide next and prev buttons
-		slideSpeed: 300,
-		paginationSpeed: 400,
-		singleItem: true
+    navigation: false, // hide next and prev buttons
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true
 
-	});
-
-
+  });
 });
